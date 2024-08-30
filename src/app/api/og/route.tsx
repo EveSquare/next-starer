1f26225d17dac9dd0d392869a2afce49f2018085
EveSquare <50450074+EveSquare@users.noreply.github.com>
2024-08-30 15:24:53 +0900

    :sparkles: reinitialize

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,4 +1,3 @@
-import OGPImage from "@/components/OGPImage";
 import { ImageResponse } from "next/og";
 
 export async function GET(req: Request) {
@@ -8,10 +7,44 @@ export async function GET(req: Request) {
     const hasTitle = searchParams.has("title");
     const title = hasTitle ? searchParams.get("title")?.slice(0, 100) : "";
 
-    return new ImageResponse(<OGPImage title={title || ""} />, {
-      width: 1200,
-      height: 630,
-    });
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: "#fff",
+            backgroundSize: "100% 100%",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            textAlign: "left",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              fontSize: 60,
+              fontStyle: "normal",
+              fontWeight: "bold",
+              color: "#000",
+              padding: "0 120px",
+              lineHeight: 1.3,
+              marginBottom: "30px",
+              wordWrap: "break-word",
+            }}
+          >
+            {title || ""}
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
   } catch (e: any) {
     console.log(`${e.message}`);
     return new Response(`Failed to generate the image`, {
